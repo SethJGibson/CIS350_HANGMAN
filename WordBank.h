4c60dd3fa52e8237f331802bc54e98846406e93d
JellyBoonz <86168103+JellyBoonz@users.noreply.github.com>
2022-06-01 15:46:37 -0400

    Add files via upload
    Header file for word bank

---
+++ WordBank.h
@@ -0,0 +1,13 @@
+#ifndef WORDBANK_H_INCLUDED
+#define WORDBANK_H_INCLUDED
+
+extern char *bank[] = {
+        "WINS","LOSE","BANK","BIKE", "KITE",
+        "FIGHT","MIGHT","BEACH","ADULT","STACK",
+        "STRING","FLIGHT","ENTITY","EMPIRE","FOLLOW",
+        "ALCOHOL","COLLEGE","CAPTURE","FICTION","DESPITE",
+        "ONGOING","OUTSIDE","PACKAGE","OVERALL","NOTHING"
+
+    };
+
+#endif // WORDBANK_H_INCLUDED
