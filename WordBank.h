5a03250d34f96c6149e30ead78e77204319466b5
Dsalo77 <salod@mail.gvsu.edu>
2022-06-02 07:09:42 -0400

    Style changes

--- WordBank.h
+++ WordBank.h
@@ -1,13 +1,12 @@
-#ifndef WORDBANK_H_INCLUDED
-#define WORDBANK_H_INCLUDED
+#ifndef WORDBANK_H_
+#define WORDBANK_H_
 
 extern char *bank[] = {
-        "WINS","LOSE","BANK","BIKE", "KITE",
-        "FIGHT","MIGHT","BEACH","ADULT","STACK",
-        "STRING","FLIGHT","ENTITY","EMPIRE","FOLLOW",
-        "ALCOHOL","COLLEGE","CAPTURE","FICTION","DESPITE",
-        "ONGOING","OUTSIDE","PACKAGE","OVERALL","NOTHING"
-
+        "WINS", "LOSE", "BANK", "BIKE", "KITE",
+        "FIGHT", "MIGHT", "BEACH", "ADULT", "STACK",
+        "STRING", "FLIGHT", "ENTITY", "EMPIRE", "FOLLOW",
+        "ALCOHOL", "COLLEGE", "CAPTURE", "FICTION", "DESPITE",
+        "ONGOING", "OUTSIDE", "PACKAGE", "OVERALL", "NOTHING"
     };
 
-#endif // WORDBANK_H_INCLUDED
+#endif  // WORDBANK_H_
